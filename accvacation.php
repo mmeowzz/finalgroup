ff69a8ad17ddf5d936b46ca3311d23fd7f25d673
mmeowzz <chanulyafernando18@gmail.com>
2024-12-04 20:54:39 +0530

    Add files via upload

---
+++ accvacation.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee</title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+<body>
+    <div class="header">
+        <!-- Back button -->
+        <div class="back-button">
+            <a href="javascript:history.back()">← Back</a>
+        </div>
+        
+        NEW SEDAWATTE TIMBER STORES
+        <button class="logout-btn">LOGOUT</button>
+    </div>
+
+    <div class="tabs">
+        <a href="accountantmain.html">Home</a>
+        <a href="accountant.html">Accountant</a>
+        <a href="acctask.php">Task</a>
+        <a href="accattendance.php">Attendance</a>
+        <a href="accsalary.html">Salary</a>
+        <a href="accovertime.html">Over Time</a>
+        <a class="active" href="accvacation.php">Vacation</a>
+    </div>
+
+    </div>
+</body>
+</html>
---
+++ shippingdoc.php
@@ -0,0 +1,30 @@
+<!--<link rel="icon" href="plazalogo.png" type="image/x-icon">-->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Employee</title>
+    <link rel = "stylesheet" type = "text/css" href = "style.css">
+</head>
+<body>
+    <div class="header">
+        <!-- Back button -->
+        <div class="back-button">
+            <a href="javascript:history.back()">← Back</a>
+        </div>
+        
+        NEW SEDAWATTE TIMBER STORES
+        <button class="logout-btn">LOGOUT</button>
+    </div>
+
+    <div class="tabs">
+        <a href="accountantmain.html">Home</a>
+        <a href="empaccountant.html">Employee</a>
+        <a class="active" href="fromsupplier.html">From Supplier</a>
+        <a href="reportfinance.html">Create Report</a>
+        <a href="suppay.html">Make Supplier Payment</a>
+        <a href="salaryies.php">Salaries</a>
+        <a href="cuspayments.php">Customer Payments</a>
+    </div>
+
+</body>
+</html>
